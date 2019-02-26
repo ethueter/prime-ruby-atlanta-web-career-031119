95df53d3e337d32049e1a5a26f0db854534724bf
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-26 20:40:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 
 def prime?(x)
-  if (2..x-1).none?{|number| number % x == 0}
+  if x < 1  
+    return false
+    els (2..x-1).none?{|number| number % x == 0}
   return true 
 else
   return false  
